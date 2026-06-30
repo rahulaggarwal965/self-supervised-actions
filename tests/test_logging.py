0b294a961d5d4514b5cfecc1fbc5784a047c4b56
Rahul Aggarwal <rahulaggarwal965@gmail.com>
2026-06-30 00:39:14 -0700

    feat: add log_figures to logger

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -5,3 +5,10 @@ def test_noop_logger_accepts_calls():
     logger = NoopLogger()
     logger.log({"loss": 1.0}, step=0)
     logger.finish()
+
+
+def test_noop_logger_accepts_log_figures():
+    from ssa.train.logging import NoopLogger
+
+    logger = NoopLogger()
+    logger.log_figures({"panel": object()}, step=0)  # must not raise
